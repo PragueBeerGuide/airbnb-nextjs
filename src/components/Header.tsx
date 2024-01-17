879cc465889ee8df3b801bee92f2bf766d6aeef7
Lukáš Vondra <vondra.ctu@gmail.com>
2024-01-17 16:34:52 +0000

    added a structure of the Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='flex justify-evenly'>
+      {/* Left section */}
+      <div>Logo</div>
+      {/* Middle section */}
+      <div>search bar</div>
+      {/* Right section */}
+      <div>Right section</div>
+    </header>
+    
+  )
+}
+
+export default Header 
